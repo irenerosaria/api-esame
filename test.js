1884b3fe569778cabb88126e124055ab2aae765e
irenerosaria <39551204+irenerosaria@users.noreply.github.com>
2019-01-15 23:18:49 +0100

    change

--- test.js
+++ test.js
@@ -3,39 +3,110 @@ const request = require('supertest');
 const app = require('./app');
 
 describe('Test read api', function(){
-    it('Sto testando la lettura degli utenti', function(done){
+
+it('Sto aggiungendo le cose da fare alla lista', function(done){
         request(app)
-            .get('/list')
+            .post('/list')
             .set('Accept', 'application/json')
-            .expect(200)
-            .end(function(err, res) {
-                if (err) return done(err);
-                assert.equal(res.body.length, 0);
-                done(); 
-            });
-    });
-    it('Sto aggiungendo gli utenti', function(done){
-        request(app)
-            .post('/list?assignedTo=mamma')
-            .set('Accept', 'application/json')
-            .send({name: 'pulire', description: 'casa',completed:true,assignedTo:"mamma"})
+            .send({name: 'pulire', description: 'casa',
+                completed:true,assignedTo:"mamma"})
             .expect(201)
             .end(function(err, res) {
                 if (err) return done(err);
+
                 done(); 
             });
     })
 
-    it('Sto aggiungendo gli utenti', function(done){
+it('Sto aggiungendo le cose da fare alla lista', function(done){
         request(app)
             .post('/list')
             .set('Accept', 'application/json')
-            .send({name: 'pulire', description: 'casa',completed:true,assignedTo:"mamma"})
-            .expect(401)
+            .send({name: 'lavare', description: 'casa',
+                completed:true,assignedTo:"papà"})
+            .expect(201)
             .end(function(err, res) {
                 if (err) return done(err);
+
                 done(); 
             });
     })
+
+
+
+
+it('Sto testando la lettura degli utenti', function(done){
+        request(app)
+            .get('/users')
+            .set('Accept', 'application/json')
+            .expect(200)
+            .end(function(err, res) {
+                if (err) return done(err);
+                assert.equal(res.body.length, 6);
+                done(); 
+            });
+    });
+
+it('Sto testando la lettura dei ToDo per utente', function(done){
+        request(app)
+            .get('/list_users?assignedTo=mamma')
+            .set('Accept', 'application/json')
+            .expect(200)
+            .end(function(err, res) {
+                if (err) return done(err);
+                assert.equal(res.body.length, 1);
+                done(); 
+            });
+    });
+
+it('Sto cancellando todo in base all id', function(done){
+        request(app)
+            .delete('/list/1')
+            .set('Accept', 'application/json')
+            .expect(200)
+            .end(function(err, res) {
+                if (err) return done(err);
+
+                assert.equal(res.body.length, 1);
+                done(); 
+            });
+    })
+it('Sto testando la lettura dei ToDo true', function(done){
+        request(app)
+            .get('/completed')
+            .set('Accept', 'application/json')
+            .expect(200)
+            .end(function(err, res) {
+                if (err) return done(err);
+                assert.equal(res.body.length,1);
+                done(); 
+            });
+    });
+it('Sto modificando lo stato di completamento in base all id', function(done){
+        request(app)
+            
+            .put('/list/0')
+            .send({completed:false})
+            .set('Accept', 'application/json')
+            .expect(201)
+            .end(function(err, res) {
+                if (err) return done(err);
+                assert.equal(res.body, false);
+                done(); 
+            });
+    })
+// it('Sto modificando la description in base all id', function(done){
+//         request(app)
+            
+//             .put('/listen/0')
+//             .send({description:'scala'})
+//             .set('Accept', 'application/json')
+//             .expect(201)
+//             .end(function(err, res) {
+//                 if (err) return done(err);
+//                 assert.equal(res.body.description, 'scala');
+//                 done(); 
+//             });
+//     })
 
 })
